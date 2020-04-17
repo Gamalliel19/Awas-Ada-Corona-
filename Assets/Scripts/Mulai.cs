7f2a36738c3c5190ae56081fff3500a6ae4058c6
gamalliel19 <sharongamalliel@gmail.com>
2020-04-17 21:55:23 +0700

    Game Jam 2020

---
+++ Assets/Scripts/Mulai.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mulai : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Quit Game");
+        }
+        
+    }
+    public void kembaliKeMenu() {
+        SceneManager.LoadScene("Main");
+    }
+    
+	    public void MulaiPermainan() {
+	        SceneManager.LoadScene("Game");
+	    }
+
+        public void KeluarPermainan()
+        {
+            Application.Quit();
+            Debug.Log("Permainan Keluar");
+        }
+  
+	   
+
+
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.M)) {
+            SceneManager.LoadScene("Main");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+	}
+}
